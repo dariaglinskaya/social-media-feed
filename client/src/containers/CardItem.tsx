95c787de9ecb2c3a5c7fc7b2a6b5663c8b590aae
Darya Hlinskaya <Darya.Hlinskaya@itechart-group.com>
2018-09-17 14:48:35 +0300

    Types

--- client/src/containers/CardItem.tsx
+++ client/src/containers/CardItem.tsx
@@ -1,7 +1,7 @@
 import { Card, Icon, Avatar } from 'antd';
 import * as React from 'react';
 
-const { Meta }: any = Card;
+const { Meta } = Card as any;
 export interface IProps {
     text: any;
     display_url: any;
