46bc13bcfa3cb51b838ac55cd5219911a487443d
Darya Hlinskaya <Darya.Hlinskaya@itechart-group.com>
2018-09-24 12:49:33 +0300

    Add search icon to input

--- client/src/containers/Search.tsx
+++ client/src/containers/Search.tsx
@@ -1,4 +1,4 @@
-import { Select } from 'antd';
+import { Select, Icon } from 'antd';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -9,6 +9,12 @@ const children: any = [];
 for (let i = 10; i < 20; i++) {
     children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
 }
+const IconText = ({ type, text }) => (
+    <span>
+        <Icon type={type} style={{ marginRight: 8 }} />
+        {text}
+    </span>
+);
 interface IProps {
     fetchDataVK?: any;
     fetchDataInst?: any;
@@ -30,14 +36,14 @@ class Search extends React.Component<IProps, IState>{
     public handleChange(value: any) {
         this.setState({ value: value });
         if (value.length === 0) {
-        } else if(value[0].substring(0,1) !== '#'){            
-                this.props.fetchDataVK(value[0]);                
-                this.props.fetchDataTwitter(value[0]);
-                this.props.fetchDataInst(value[0]);
-        } else if(value[0].substring(0,1) === '#'){
-                this.props.fetchDataVK(value[0].substring(1));                
-                this.props.fetchDataTwitter(value[0].substring(1));
-                this.props.fetchDataInst(value[0].substring(1));
+        } else if (value[0].substring(0, 1) !== '#') {
+            this.props.fetchDataVK(value[0]);
+            this.props.fetchDataTwitter(value[0]);
+            this.props.fetchDataInst(value[0]);
+        } else if (value[0].substring(0, 1) === '#') {
+            this.props.fetchDataVK(value[0].substring(1));
+            this.props.fetchDataTwitter(value[0].substring(1));
+            this.props.fetchDataInst(value[0].substring(1));
         }
     }
     public render() {
@@ -45,8 +51,11 @@ class Search extends React.Component<IProps, IState>{
             <div>
                 <Select
                     mode="tags"
+                    autoFocus={true}
+                    showArrow={true}
+                    showSearch={true}
                     style={{ width: '100%' }}
-                    placeholder="Search by tag. #minsk #nasa #programming"
+                    placeholder={<IconText type="search" text="Search by tag. #minsk #nasa #programming" />}
                     onChange={this.handleChange.bind(this)}
                 />
             </div>
